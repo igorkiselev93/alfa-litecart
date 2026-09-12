02e5c792bbc6c6a9e8a7fa1ec6a8e9dcbb4a06c7
Kiselev <ikiselev@elinext.com>
2026-09-12 19:47:58 +0300

    chore: add playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30_000,
+  expect: { timeout: 5_000 },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [
+    ['list'],
+  ],
+  use: {
+    baseURL: 'https://litecart.stqa.ru',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        channel: 'chrome',
+        launchOptions: {
+          executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+        },
+      },
+    },
+  ],
+});
