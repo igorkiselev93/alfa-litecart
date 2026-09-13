ed6af982816f9df4f0726028a815bb563a7ac463
Kiselev <ikiselev@elinext.com>
2026-09-13 16:44:36 +0300

    feat: add LoginPage

---
+++ pages/LoginPage.ts
@@ -0,0 +1,39 @@
+import { Page, Locator } from '@playwright/test';
+import { BasePage } from './BasePage';
+
+export class LoginPage extends BasePage {
+  // CSS selectors
+  readonly emailInput: Locator;
+  readonly passwordInput: Locator;
+  readonly loginButton: Locator;
+  // XPath selector (project requirement)
+  readonly errorNotice: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.emailInput = page.locator('input[name="email"]');
+    this.passwordInput = page.locator('input[name="password"]');
+    this.loginButton = page.locator('button[name="login"]');
+    // XPath: error notice div with class "notice errors"
+    this.errorNotice = page.locator('xpath=//div[contains(@class,"notice") and contains(@class,"errors")]');
+  }
+
+  async goto(): Promise<void> {
+    await super.goto('/en/login');
+  }
+
+  async login(email: string, password: string): Promise<void> {
+    await this.emailInput.fill(email);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+
+  async getErrorText(): Promise<string> {
+    return (await this.errorNotice.textContent()) ?? '';
+  }
+
+  isLoggedIn(): Locator {
+    // Sidebar shows "Logout" link when authenticated
+    return this.page.locator('a[href*="logout"]');
+  }
+}
